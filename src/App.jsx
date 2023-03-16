d16b7ccf948a24a00733b7f53c2a72197074dcc9
ikechukwu11 <kevinuzoma@gmail.com>
2023-03-16 23:51:20 +0100

    More updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,59 @@
-import './App.css'
-import Board from './components/Board/Board'
-import { useState } from 'react'
-import Editable from './components/Editable/Editable'
+import "./App.css";
+import Board from "./components/Board/Board";
+import { useState } from "react";
+import Editable from "./components/Editable/Editable";
 //import boarddata from '../data/boarddata'
 
 function App() {
   const [boards, setBoards] = useState([
     {
       id: Date.now() + Math.random() * 2,
-      title: 'To Do',
+      title: "To Do",
       cards: [
         {
           id: Date.now() + Math.random(),
-          title: 'Card 1',
+          title: "Card 1",
           tasks: [],
           labels: [
             {
-              text: 'frontend',
-              color: 'blue',
+              text: "frontend",
+              color: "blue",
             },
           ],
-          desc: 'This is Card 1',
-          date: '',
+          desc: "This is Card 1",
+          date: "",
         },
 
         {
           id: Date.now() + Math.random(),
-          title: 'Card 2',
+          title: "Card 2",
           tasks: [],
           labels: [
             {
-              text: 'backend',
-              color: 'black',
+              text: "backend",
+              color: "black",
             },
           ],
-          desc: 'This is Card 2',
-          date: '',
+          desc: "This is Card 2",
+          date: "",
         },
 
         {
           id: Date.now() + Math.random(),
-          title: 'Card 3',
+          title: "Card 3",
           tasks: [],
           labels: [
             {
-              text: 'devops',
-              color: 'teal',
+              text: "devops",
+              color: "teal",
             },
           ],
-          desc: 'This is Card 3',
-          date: '',
+          desc: "This is Card 3",
+          date: "",
         },
       ],
     },
-  ])
+  ]);
 
   const addCard = (title, bid) => {
     const card = {
@@ -61,47 +61,65 @@ function App() {
       title,
       labels: [],
       tasks: [],
-      desc: '',
-      date: '',
-    }
+      desc: "",
+      date: "",
+    };
 
-    const index = boards.findIndex((item) => item.id === bid)
-    if (index < 0) return
-    const tempBoards = [...boards]
-    tempBoards[index].cards.push(card)
-    setBoards(tempBoards)
-  }
+    const index = boards.findIndex((item) => item.id === bid);
+    if (index < 0) return;
+    const tempBoards = [...boards];
+    tempBoards[index].cards.push(card);
+    setBoards(tempBoards);
+  };
 
   const removeCard = (cid, bid) => {
-    const bIndex = boards.findIndex((item) => item.id === bid)
-    if (bIndex < 0) return
+    const bIndex = boards.findIndex((item) => item.id === bid);
+    if (bIndex < 0) return;
 
-    const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid)
-    if (cIndex < 0) return
-    const tempBoards = [...boards]
-    tempBoards[bindex].cards.splice(cIndex, 1)
-    setBoards(tempBoards)
-  }
+    const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
+    if (cIndex < 0) return;
+    const tempBoards = [...boards];
+    tempBoards[bindex].cards.splice(cIndex, 1);
+    setBoards(tempBoards);
+  };
+
+  const addBoard = (title) => {
+    setBoards([
+      ...boards,
+      {
+        id: Date.now() + Math.random() * 2,
+        title: title,
+        cards: [],
+      },
+    ]);
+  };
+
+  const removeBoard = (bid) => {
+    const tempBoards = boards.filter((item) => item.id !== bid);
+    setBoards(tempBoards);
+  };
 
   return (
-    <div className='app'>
-      <div className='app_navbar'>
+    <div className="app">
+      <div className="app_navbar">
         <h2>Kanban</h2>
       </div>
-      <div className='app_outer'>
-        <div className='app_boards'>
+      <div className="app_outer">
+        <div className="app_boards">
           {boards.map((item) => (
             <Board key={item.id} board={item} />
           ))}
           <Editable
-            displayClass='app_boards_board_add'
-            text='Add Board'
-            placeholder='Enter board title'
+            displayClass="app_boards_board_add"
+            buttonText="Add Board"
+            text="Add Board"
+            placeholder="Enter board title"
+            onSubmit={(value) => addBoard(value)}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
